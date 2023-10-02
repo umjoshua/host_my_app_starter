841ab1b2b9f7ef8fac022408f134e6ce743efe48
Joshua Mathew <umjoshua@gmail.com>
2023-10-02 08:17:23 +0530

    test

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const cors = require('cors')
-
+const { exec } = require('child_process')
 const app = express()
 
 app.use(cors())
 
-app.use('/',express.static('public'))
-app.use('/test',(req,res)=>{
+app.use('/', express.static('public'))
+app.use('/test', (req, res) => {
+
+    exec("ls", (err, stdout, stderr) => {
+        if(err) console.log(err)
+        if(stdout) console.log(stdout)
+        if(stderr) console.log(stderr)
+
+    })
+    exec("cat index.js", (err, stdout, stderr) => {
+        if(err) console.log(err)
+        if(stdout) console.log(stdout)
+        if(stderr) console.log(stderr)
+
+    })
     res.send("<h1>Testing</h1>")
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening")
 })
