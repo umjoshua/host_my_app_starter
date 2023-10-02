98135336d72d4ff86718094666e4999bcd211b49
Joshua Mathew <umjoshua@gmail.com>
2023-10-02 07:46:00 +0530

    test

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 app.use(cors())
 
 app.use('/',express.static('public'))
+app.use('/test',(req,res)=>{
+    res.send("<h1>Testing</h1>")
+})
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Listening")
